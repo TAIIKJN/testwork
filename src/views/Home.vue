f04c9ef86a52b1e321a180768f6ae422e2e7095e
TAIIKJN <taii.kanjana@gmail.com>
2021-08-06 17:15:01 +0700

    setup

---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <b-container>
+      <b-row>
+        <b-col lg="6" class="my-1">
+          <b-form-input
+            id="filter-input"
+            v-model="filter"
+            type="search"
+            placeholder="ค้นหา"
+          ></b-form-input>
+        </b-col>
+      </b-row>
+      <b-table
+        :items="items"
+        :filter="filter"
+        :filter-included-fields="filterOn"
+        stacked="md"
+        show-empty
+        small
+        @filtered="onFiltered"
+      >
+        <template #cell(name)="row">
+          {{ row.value.first }} {{ row.value.last }}
+        </template>
+      </b-table>
+    </b-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
